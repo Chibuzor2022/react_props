1e0b558e7ce82933b1cd141b1493a2e8e62cdee9
chibuzor <incovenant@hotmail.com>
2025-04-09 01:05:45 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import PlayerList from "./components/PlayerList";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			{/* Heading */}
+			<h1 className="my-4">Football Players</h1>
+			{/* Imported PLayerList */}
+			<PlayerList />
+		</div>
+	);
 }
 
 export default App;
